b44a851c4dfc080207d239ac19c64acfce037528
Kuang Chen <33445752+kchenphy@users.noreply.github.com>
2018-07-02 13:46:57 -0700

    Move methods with side-effects into Dsl.scala

---
+++ core/src/test/scala/better_paths/common/TempPathProvider.scala
@@ -0,0 +1,14 @@
+package better_paths.common
+
+import org.apache.hadoop.fs.Path
+import org.scalatest.{BeforeAndAfterEach, Suite}
+
+trait TempPathProvider extends BeforeAndAfterEach {
+  self: Suite =>
+  var tmpPath: Path = _
+
+  override protected def beforeEach(): Unit = {
+    super.beforeEach()
+    tmpPath = new Path(System.getProperty("java.io.tmpdir"), System.nanoTime().toString)
+  }
+}
