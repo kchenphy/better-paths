bb3b9475afb44d606a50ff0fc28b1eff468bbc5a
Kuang Chen <33445752+kchenphy@users.noreply.github.com>
2018-07-02 14:46:12 -0700

    Introduce emptiness for path, and other minor refactors

--- core/src/test/scala/better_paths/DslTest.scala
+++ core/src/test/scala/better_paths/DslTest.scala
@@ -2,30 +2,42 @@ package better_paths
 
 import java.io.IOException
 
-import better_paths.Dsl.{delete, mkdirs, touch}
+import better_paths.Dsl.{delete, mkdirs, touch, touchz}
 import better_paths.common.{TempPathProvider, TestMiniDFSCluster}
 import better_paths.scalatest_sugar.PathSugar
 import org.apache.hadoop.fs.Path
-import org.scalatest.enablers.Existence
 import org.scalatest._
+import org.scalatest.enablers.Existence
 
 class DslTest extends FlatSpec with Matchers with TempPathProvider with PathSugar with TestMiniDFSCluster
   with Inspectors {
 
-  implicit val pathExistence: Existence[Path] = existIn(fs)
+  implicit val pathExistence: Existence[Path] = existence(fs)
 
   "touch" should "create a zero-length file only when file is non-existing, or do nothing if file already exists" in {
     touch(tmpPath / "a")
     tmpPath / "a" should exist
+    tmpPath / "a" shouldBe empty
+  }
+
+  "touchz" should "create a zero-length file, or throw IOException if file already exists" in {
+    val path = tmpPath / "a"
+    touchz(path)
+    path should exist
+    path shouldBe empty
+
+    intercept[IOException] {
+      touchz(path)
+    }
   }
 
   "mkdirs" should "create paths recursively" in {
     mkdirs(tmpPath / "a" / "b")
-    tmpPath / "a" / "b" should be a directory
+    (tmpPath / "a" / "b") should be a directory
+    (tmpPath / "a" / "b") shouldBe empty
   }
 
-
-  private def setupPaths(root: Path) = {
+  private def setupPaths(root: Path): Seq[Path] = {
     touch(root / "a")
     mkdirs(root / "b")
     touch(root / "c" / "d")
