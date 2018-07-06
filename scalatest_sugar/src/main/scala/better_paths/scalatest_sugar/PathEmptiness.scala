0f370dee515b6f0eaf0bdf0c4151dc9402cb6a6c
Kuang Chen <33445752+kchenphy@users.noreply.github.com>
2018-07-06 14:06:52 -0700

    Use pattern matching in pathEmptiness

--- scalatest_sugar/src/main/scala/better_paths/scalatest_sugar/PathEmptiness.scala
+++ scalatest_sugar/src/main/scala/better_paths/scalatest_sugar/PathEmptiness.scala
@@ -1,17 +1,19 @@
 package better_paths.scalatest_sugar
 
+import better_paths.{IsDirectory, IsFile}
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.scalatest.enablers.Emptiness
 
 trait PathEmptiness {
   implicit def pathEmptiness(implicit fs: FileSystem): Emptiness[Path] =
     new Emptiness[Path] {
+
       override def isEmpty(thing: Path): Boolean = {
-        assert(fs.exists(thing))
-        val status = fs.getFileStatus(thing)
-        if (status.isDirectory) fs.listStatus(thing).isEmpty
-        else if (status.isFile) status.getLen == 0L
-        else isEmpty(fs.resolvePath(thing))
+        thing match {
+          case IsFile()      => fs.getFileStatus(thing).getLen == 0
+          case IsDirectory() => fs.listStatus(thing).isEmpty
+          case _             => isEmpty(fs.resolvePath(thing))
+        }
       }
     }
 }
