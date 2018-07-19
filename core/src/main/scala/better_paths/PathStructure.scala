4ffdba958f6d2615a7aadcbc289686f2968ad67f
Kuang Chen <33445752+kchenphy@users.noreply.github.com>
2018-07-18 22:29:03 -0700

    Add walk() to PathStructureSpec

--- core/src/main/scala/better_paths/PathStructure.scala
+++ core/src/main/scala/better_paths/PathStructure.scala
@@ -2,9 +2,32 @@ package better_paths
 
 import org.apache.hadoop.fs.{FileSystem, Path}
 
+import scala.collection.mutable
+
 object PathStructure {
 
+  private class PreOrderIterator(root: Path)(implicit fs: FileSystem)
+      extends Iterator[Path] {
+    private val stack: mutable.Stack[Path] = mutable.Stack(root)
+
+    override def hasNext: Boolean = stack.nonEmpty
+
+    override def next(): Path = {
+      val h = stack.pop()
+      h match {
+        case IsDirectory() =>
+          stack.pushAll(
+            fs.listStatus(h)
+              .map(_.getPath)
+              .sorted(Ordering.by[Path, String](_.toString).reverse))
+        case _ =>
+      }
+      h
+    }
+  }
+
   trait Implicits {
+
     implicit class PathStructureOps(path: Path) {
       def parent: Path = path.getParent
 
@@ -13,6 +36,9 @@ object PathStructure {
 
       def contains(child: Path)(implicit fs: FileSystem): Boolean =
         PathContains(path, child)
+
+      def descendantsIterator(implicit fs: FileSystem): Iterator[Path] =
+        new PreOrderIterator(path)
     }
   }
 
